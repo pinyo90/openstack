06c1a9ec671e42de83f238101fdce875013280ff
pinyo90 <erwin.cip@gmail.com>
2014-07-18 02:36:35 +0700

    Create persiapan.sh

---
+++ persiapan.sh
@@ -0,0 +1,34 @@
+rm /etc/hostname
+echo "server1" | tee /etc/hostname
+
+rm /etc/hosts
+echo "127.0.0.1 localhost
+${INTERNAL_IP} server1
+" | tee /etc/hosts
+
+rm /etc/network/interfaces
+echo "
+# localhost
+auto lo
+iface lo inet loopback
+
+# public IP
+auto eth0
+iface eth0 inet static
+	address ${INTERNAL_IP}
+	netmask 255.255.255.0
+	gateway ${INTERNAL_GW}
+	dns-nameservers 8.8.8.8
+
+auto eth1
+iface eth1 inet manual
+	up ifconfig $IFACE 0.0.0.0 up
+	up ifconfig $IFACE promisc
+" | tee /etc/network/interfaces
+
+echo "
+export INTERNAL_IP=${INTERNAL_IP}
+export INTERNAL_GW=${INTERNAL_GW}
+" >> /root/.bashrc
+
+shutdown -h now
